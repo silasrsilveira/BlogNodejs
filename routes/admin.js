3ca90840062870350e28d7d257ba914caad6af5d
Silas Ribeiro <silass@gmail.com>
2021-02-08 18:21:58 -0400

    Configurado a const Categoria

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,11 @@
 const express = require("express")
 const router = express.Router()
 
+//Importando mongoose
+const mongoose = require("mongoose")
+require ("../models/Categoria")
+const Categoria = mongoose.model("categorias")
+
 // Rota principal para o painel adm
 router.get('/',(req, res) => {
     res.render("admin/index")
@@ -16,6 +21,20 @@ router.get("/categorias",(req, res) =>{
 
 router.get('/categorias/add', (req, res) => {
     res.render("admin/addcategorias")
+})
+
+router.post("/categorias/nova", (req, res) => {
+    const novaCategoria = {
+        nome: req.body.nome,
+        slug: req.body.slug
+    }
+
+    new Categoria(novaCategoria).save().then(() => {
+        console.log("Categoria salva com sucesso");
+    }).catch((err) =>{
+        console.log("Erro ao salvar categoria"+err);
+    })
+
 })
 
 module.exports = router
