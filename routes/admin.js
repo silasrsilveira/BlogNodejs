f95c16b2ecd58034bc5b081fe5bc902baa466770
Silas Ribeiro <silass@gmail.com>
2021-02-08 23:24:45 -0400

    Criado button de editar e configurado a opção para edi~~

--- routes/admin.js
+++ routes/admin.js
@@ -59,4 +59,38 @@ router.post("/categorias/nova", (req, res) => {
     }
 })
 
+// Rota para editar categorias (ADICIONADO O LEAN)
+router.get("/categorias/edit/:id", (req, res) => {
+    Categoria.findOne({_id: req.params.id}).lean().then((categoria) => {
+        res.render("admin/editcategorias", {categoria: categoria})
+    }).catch((err => {
+        req.flash("error_msg", "Esta categoria não existe")
+        res.redirect("/admin/categorias")
+    }))
+})
+
+router.post("/categorias/edit", (req, res) =>{
+ 
+Categoria.findOne({_id: req.body.id}).then((categoria) => {
+
+    categoria.nome = req.body.nome
+    categoria.slug = req.body.slug
+
+    categoria.save().then((categoria) => {
+        req.flash("success_msg", "Categoria criada com sucesso")
+        res.redirect("/admin/categorias")
+    }).catch((err) => {
+        req.flash("error_msg", "Houve um erro interno ao salvar a categoria")
+        res.redirect("/admin/categorias")
+    })
+
+
+}).catch((err) => {
+    req.flash("error_msg", "Houve um erro ao editar a categoria!")
+    res.redirect("/admin/categorias")
+})
+
+})
+
+
 module.exports = router
