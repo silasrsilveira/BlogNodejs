b9b84f14fac902f261919f90b1852709d2a99877
Silas Ribeiro <silass@gmail.com>
2021-02-08 20:35:13 -0400

    Configurando a listagem de Categorias

--- routes/admin.js
+++ routes/admin.js
@@ -16,7 +16,13 @@ router.get('/posts',(req, res) => {
 })
 
 router.get("/categorias",(req, res) =>{
-    res.render("admin/categorias")
+    Categoria.find().sort({date: "desc"}).lean().then((categorias) => {
+        res.render("admin/categorias", {categorias: categorias})
+    }).catch((err) => {
+        req.flash("error_msg", "Erro ao listar as categorias")
+        res.redirect("/admin")
+    })
+    
 })
 
 router.get('/categorias/add', (req, res) => {
