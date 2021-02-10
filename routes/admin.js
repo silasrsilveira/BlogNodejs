c9114c618176574ca962188d20033141e6580a6c
Silas Ribeiro <silass@gmail.com>
2021-02-09 20:26:41 -0400

    Adicionado a categoria DELETAR

--- routes/admin.js
+++ routes/admin.js
@@ -92,5 +92,15 @@ Categoria.findOne({_id: req.body.id}).then((categoria) => {
 
 })
 
+router.post("/categorias/deletar", (req, res) => {
+    Categoria.remove({_id: req.body.id}).then(() => {
+        req.flash("success_msg", "Categoria deletada com sucesso!")
+        res.redirect("/admin/categorias")
+    }).catch((err) => {
+        req.flash("error_msg", "Erro ao deletar a categoria")
+        res.redirect("/admin/categorias")
+    })
+})
+
 
 module.exports = router
